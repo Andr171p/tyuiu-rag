9e0c7f9188bf8d2b96f7dabde0ae89a9c687a11e
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-01-04 03:43:14 +0500

    [tyuiu-rag-llm] 04.01.2025

---
+++ main.py
@@ -0,0 +1,39 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredWordDocumentLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+# from langchain.retrievers import BM25Retriever, EnsembleRetriever
+# from langchain.vectorstores.faiss import FAISS
+
+from chromadb.config import Settings
+from langchain.vectorstores import Chroma
+
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=100
+)
+
+loader = UnstructuredWordDocumentLoader(r"C:\Users\andre\TyuiuRAG\documents\tyuiu-rag.docx")
+split_docx = loader.load_and_split(text_splitter)
+
+# print(split_docx[1])
+
+model_name = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+embedding = HuggingFaceEmbeddings(model_name=model_name,
+                                  model_kwargs=model_kwargs,
+                                  encode_kwargs=encode_kwargs)
+
+# vector_store = FAISS.from_documents(split_docx, embedding=embedding)
+persist_directory = r"C:\Users\andre\TyuiuRAG\chromadb_data"
+db = Chroma.from_documents(
+    split_docx,
+    embedding,
+    client_settings=Settings(anonymized_telemetry=False),
+)
+db.save_local("chroma_db")
+
+question = "Направления строительного института"
+docs = db.similarity_search(question, k=4)
+print(docs[0])
